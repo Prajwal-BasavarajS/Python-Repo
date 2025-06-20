c319bae689251956f8fa7c2cfe7b59fa8c6d942e
Prajwal <prajwalbasavaraj13@gmail.com>
2025-06-20 10:11:22 +0200

    first commit

---
+++ Conditional_Statements/1_Conditional.py
@@ -0,0 +1,21 @@
+# if 
+age = 19
+
+if age >= 18 :
+    print("elegible to vote")
+
+
+# short hand if 
+wage = 22
+if wage >= 21 : print(" girls can marry ")
+
+
+# elif 
+
+if age >= 20 :
+    print("Allowed inside the PUB!! ")
+elif age <= 19 :
+    print("Use brains and edit the AADHAR card ")
+else :
+    print("Not allowed inside the PUB!! BYE BYE!! ")
+
