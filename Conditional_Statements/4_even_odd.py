eddc7ec2e323077e475d252167b433ee20531da6
Prajwal <prajwalbasavaraj13@gmail.com>
2025-06-20 11:10:53 +0200

    first commit

---
+++ Conditional_Statements/4_even_odd.py
@@ -0,0 +1,15 @@
+x =  int(input("enter a number for even , odd \n "))
+
+if (x%2 == 0):
+    print('even')
+else :
+    print('odd')
+
+#  =  int(input("enter a number for even , odd \n "))
+
+
+for i in range(1,x+1):
+    if (i % 2 == 0 ):
+        print(f'{i} is even')
+    else :
+        print(f'{i} is odd')
