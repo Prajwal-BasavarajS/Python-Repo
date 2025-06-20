e2c1112dcdcb6cd5af78b9976da190db77dac87a
Prajwal <prajwalbasavaraj13@gmail.com>
2025-06-20 11:24:15 +0200

    updated commit

--- Conditional_Statements/5_check_flag_status.py
+++ Conditional_Statements/5_check_flag_status.py
@@ -1,18 +1,2 @@
 a =int(input('enter number \n'))
 b = int(input('enter number \n'))
-# a =int(input('enter number \n'))
-# b = int(input('
-
-def checkStatus(a, b, flag):
-        # code here
-        if flag:
-            return a < 0 and b < 0
-        else:
-            return (a >= 0) != (b >= 0)
-
-
-a=4 
-b=-1
-flag = True       
-res = checkStatus(a,b,flag)
-print(res)
