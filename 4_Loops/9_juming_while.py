ee66b8da92b4771ecadf5ed6876cf8341da50369
Prajwal-BasavarajS <prajwalbasavaraj13@gmail.com>
2025-06-26 18:23:15 +0200

    inital commit

---
+++ 4_Loops/9_juming_while.py
@@ -0,0 +1,15 @@
+# i=1
+# while(i<=8):
+#          ##Your code here
+#         s = i*i
+#         print ( s , end = " ")
+#         i+=1
+
+def powers(x):
+        i = 1
+        while(i<=x):
+            s = i*i
+            print ( s , end = " ")
+            i+=1
+
+powers(19)
