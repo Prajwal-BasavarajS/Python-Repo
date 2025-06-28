7e30a8ab6f76d54b4c6d50c7b2f431d54898d219
Prajwal-BasavarajS <prajwalbasavaraj13@gmail.com>
2025-06-28 13:36:24 +0200

    secondary commit

--- 4_Loops/Pattern_printing/1_square_wall.py
+++ 4_Loops/Pattern_printing/1_square_wall.py
@@ -2,3 +2,13 @@ for i in range(0,5):
     for j in range(1,6):
         print('*',end = " ")
     print()
+
+
+
+
+
+for i in range(7):
+    for j in range (7):
+        print("*", end = " ")
+    
+    print()
