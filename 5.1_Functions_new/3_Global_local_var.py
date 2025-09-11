1c74ad25637caf5dea255c8d50ada80c7ba775f3
Prajwal-BasavarajS <prajwalbasavaraj13@gmail.com>
2025-09-11 07:50:49 +0200

    seconf commit

--- 5.1_Functions_new/3_Global_local_var.py
+++ 5.1_Functions_new/3_Global_local_var.py
@@ -22,3 +22,20 @@ print("Global a = ",a)
 g()
 print("Global a = ",a)
 h()
+
+
+
+
+class EmptyClass:
+    pass  # No methods or attributes yet
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    
+    def greet(self):
+        pass  # Placeholder for greet method
+
+# Creating an instance of the class
+p = Person("Emily", 30)
