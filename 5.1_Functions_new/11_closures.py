7b946abfc1a32e2531ec91610654b70bd34399a7
Prajwal-BasavarajS <prajwalbasavaraj13@gmail.com>
2025-10-06 20:38:24 +0200

    intial commit

--- 5.1_Functions_new/11_closures.py
+++ 5.1_Functions_new/11_closures.py
@@ -10,3 +10,36 @@ closure = outer_function(10)
 # Call the closure with different values of 'y'
 print(closure(5)) 
 print(closure(20))
+
+
+
+
+def fun1(): 
+    a = 45
+    def fun2(): 
+        nonlocal a 
+        a=54
+        print(a)
+    fun2()
+    print(a)
+fun1()
+
+
+
+
+a = "I am global"
+
+def outer():
+    b = "I am enclosing"
+    
+    def inner():
+        global a
+        nonlocal b
+        a = "Modified global"
+        b = "Modified enclosing"
+    
+    inner()
+    print("Inside outer:", b)
+    
+outer()
+print("Outside outer:", a)
