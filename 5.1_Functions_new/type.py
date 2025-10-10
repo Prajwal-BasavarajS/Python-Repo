a7ede36f6d980ea5281169fdbe4342b402c3136d
Prajwal-BasavarajS <prajwalbasavaraj13@gmail.com>
2025-10-10 20:23:24 +0200

    changes in type

--- 5.1_Functions_new/type.py
+++ 5.1_Functions_new/type.py
@@ -1,2 +1,16 @@
 x = 3
 print(type(x))
+
+
+
+def add(t):
+    def combine(p):
+        return t + "doesn't like " + p 
+    return combine
+
+love = add("Macbook ")
+
+i = love("Charging")
+print(i)
+
+print(love.__closure__[0].cell_contents, "Closure \n")
