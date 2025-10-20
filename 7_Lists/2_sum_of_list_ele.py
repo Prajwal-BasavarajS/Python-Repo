4d1e94ca5d4c6ea1fd9f4bf863ea73c350bc6969
Prajwal-BasavarajS <prajwalbasavaraj13@gmail.com>
2025-10-20 23:57:51 +0200

    intial commit

---
+++ 7_Lists/2_sum_of_list_ele.py
@@ -0,0 +1,7 @@
+i = [3,5,24,24,24]
+sum = 0
+for li in i :
+    sum += int(li)
+    print(sum)
+print(sum)
+
---
+++ 7_Lists/3_decrease_each_ele_by_one.py
@@ -0,0 +1,7 @@
+i = [3,5,24,24,24]
+print(i)
+f = []
+for z in i :
+    f.append(z-1)
+
+print(f)
